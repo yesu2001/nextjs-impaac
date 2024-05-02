1382ceb994979d133a1aafcced8d786de915d2ae
Yesu <yesupadam2001@gmail.com>
2024-05-02 17:07:06 +0530

    second commit

--- app/(catalogue)/layout.js
+++ app/(catalogue)/layout.js
@@ -1,12 +1,28 @@
 import MainLayout from "@/layouts/main/MainLayout";
 import Providers from "@/components/Providers";
+import { seoPageData } from "@/_mock/seoPageData";
+import { SocialLink } from "@/_mock/socialLink";
 
 export const metadata = {
   title: {
     template: "%s | Impaac Foundation",
     default: "Impaac Foundation → India's Only Non-profit Crowdfunding",
   },
-  description: "Impaac Foundation → India's Only Non-profit Crowdfunding",
+  description: seoPageData.home.description,
+  openGraph: {
+    locale: "en_US",
+    type: "article",
+    site_name: "Impaac Foundation",
+    description: seoPageData.home.description,
+    articlePublisher: SocialLink.facebookLink,
+  },
+  twitter: {
+    card: "summary",
+    site: "@impaacidea",
+    title: "Impaac Foundation",
+    description: seoPageData.home.description,
+    creator: "@impaacidea",
+  },
 };
 
 export default function CatalogueLayout({ children }) {
